6ff1730de058ab33592e750220106ded3f8e167a
Luca Zenari <lozeno1982@gmail.com>
2014-01-06 07:29:33 +0000

    Changed mainwindow layout to include a Ribbon, and changed BaseWindow baseclass to inherit from RibbonWindow

--- GestionePosizioni/GestionePosizioni/BaseClasses/BaseWindow.cs
+++ GestionePosizioni/GestionePosizioni/BaseClasses/BaseWindow.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls.Ribbon;
 using QueryManager;
 
 namespace GestionePosizioni.BaseClasses
 {
-    public class BaseWindow : Window
+    public class BaseWindow : RibbonWindow
     {
         public IDataStorage DataStorage
         {
