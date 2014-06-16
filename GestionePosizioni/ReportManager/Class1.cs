a1f3514b272eab1bf4145b7da9e2492621df21b2
Luca Zenari <lozeno1982@gmail.com>
2014-06-16 20:05:36 +0100

    Revert "some reference optimization"
    This reverts commit ed4bb37a6d12a04393594a5c1baac5aae77d0e62.

--- GestionePosizioni/ReportManager/Class1.cs
+++ GestionePosizioni/ReportManager/Class1.cs
@@ -1,4 +1,10 @@
-﻿namespace ReportManager
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportManager
 {
     public class Class1
     {
