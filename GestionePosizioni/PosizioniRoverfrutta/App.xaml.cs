0b0542ae8bb72e144920b580f153f2b6140847b7
Luca Zenari <lozeno1982@gmail.com>
2014-06-16 21:08:14 +0100

    New Application Project
    Going for a fresh and possibly better start with a new multi-windows
    approach; updated several libraries as well.

---
+++ GestionePosizioni/PosizioniRoverfrutta/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using QueryManager;
+
+namespace PosizioniRoverfrutta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IDataStorage DataStorage { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitializeDataStorage();
+
+            SetApplicationBehaviour();
+
+            ShowMainWindow();
+        }
+
+        private void ShowMainWindow()
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+
+        private void SetApplicationBehaviour()
+        {
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        private void InitializeDataStorage()
+        {
+            DataStorage = new RavenDataStorage();
+            DataStorage.Initialize();
+        }
+    }
+}
