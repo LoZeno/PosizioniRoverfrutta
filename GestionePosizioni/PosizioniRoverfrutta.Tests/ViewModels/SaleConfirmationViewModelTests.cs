faee3e7a4fb941db98c60b39be3b43130aa2a4ac
Luca Zenari <luca.zenari@thetrainline.com>
2014-10-01 15:01:29 +0100

    basic VM structure

---
+++ GestionePosizioni/PosizioniRoverfrutta.Tests/ViewModels/SaleConfirmationViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PosizioniRoverfrutta.Tests.ViewModels
+{
+    [TestFixture]
+    public class SaleConfirmationViewModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}
---
+++ GestionePosizioni/PosizioniRoverfrutta/ViewModels/SaleConfirmationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PosizioniRoverfrutta.Annotations;
+
+namespace PosizioniRoverfrutta.ViewModels
+{
+    public class SaleConfirmationViewModel : INotifyPropertyChanged
+    {
+        public SaleConfirmationViewModel()
+        {
+            CustomerControlViewModel = new CustomerControlViewModel();
+            ProviderControlViewModel = new CustomerControlViewModel();
+            //inizializzare viewmodel del trasportatore
+            //viewmodel dei prodotti
+            //viewmodel del documento vero e proprio
+        }
+
+        public CustomerControlViewModel CustomerControlViewModel { get; private set; }
+
+        public CustomerControlViewModel ProviderControlViewModel { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
