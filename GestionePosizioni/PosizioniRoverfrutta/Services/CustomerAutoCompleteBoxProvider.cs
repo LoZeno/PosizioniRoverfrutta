fb77d2427c541ee4f5e72442bcb7fd904356de4d
Luca Zenari <luca.zenari@thetrainline.com>
2014-10-03 16:14:38 +0100

    converted the autocompleteboxprovider to use extension methods for Session instead of repositories

---
+++ GestionePosizioni/PosizioniRoverfrutta/Services/CustomerAutoCompleteBoxProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dragonz.actb.provider;
+using Models;
+using QueryManager;
+using QueryManager.QueryHelpers;
+
+namespace PosizioniRoverfrutta.Services
+{
+    class CustomerAutoCompleteBoxProvider : IAutoCompleteWithReturnValueDataProvider
+    {
+        private readonly IDataStorage _dataStorage;
+        private Dictionary<string, Customer> _customerTemporaryStorage = new Dictionary<string, Customer>();
+
+
+        public CustomerAutoCompleteBoxProvider(IDataStorage dataStorage)
+        {
+            _dataStorage = dataStorage;
+        }
+
+        public object GetValue(string selectedText)
+        {
+            if (_customerTemporaryStorage.ContainsKey(selectedText))
+            {
+                return _customerTemporaryStorage[selectedText];
+            }
+            return null;
+        }
+
+        public IEnumerable<string> GetItems(string textPattern)
+        {
+            if (textPattern.Length > 4)
+            {
+                using (var newSession = _dataStorage.CreateSession())
+                {
+                    _customerTemporaryStorage = newSession.FindByPartialName<Customer>(textPattern)
+                        .Take(30)
+                        .ToDictionary(x => x.CompanyName, x => x);
+                }
+            }
+            else
+            {
+                _customerTemporaryStorage = new Dictionary<string, Customer>();
+            }
+            return _customerTemporaryStorage.Keys;
+        }
+    }
+}
---
+++ GestionePosizioni/QueryManager/QueryHelpers/CompanyQueryHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Models;
+using Raven.Client;
+
+namespace QueryManager.QueryHelpers
+{
+    public static class CompanyQueryHelper
+    {
+        public static IEnumerable<T> FindByPartialName<T>(this IDocumentSession session, string partialName) where T : CompanyBase
+        {
+            var objectName = typeof(T).Name;
+            var query = session.Query<T>(objectName + "/ByCompanyName");
+            return partialName.Split(' ').Aggregate(query, (current, term) => current.Search(c => c.CompanyName, "*" + term + "*", options: SearchOptions.And, escapeQueryOptions: EscapeQueryOptions.AllowAllWildcards));
+        }
+    }
+}
