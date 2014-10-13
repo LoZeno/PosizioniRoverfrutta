1ffff617b5fa27668ac3b6392413e83f63781f3b
Luca Zenari <luca.zenari@thetrainline.com>
2014-10-13 16:12:10 +0100

    preliminary work for the products grid

---
+++ GestionePosizioni/Models/ProductDescription.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class ProductDescription
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; } 
+    }
+}
---
+++ GestionePosizioni/PosizioniRoverfrutta/Services/ProductNamesAutoCompleteBoxProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using dragonz.actb.provider;
+using Models;
+using QueryManager;
+using Raven.Client.Linq;
+
+namespace PosizioniRoverfrutta.Services
+{
+    public class ProductNamesAutoCompleteBoxProvider : IAutoCompleteDataProvider
+    {
+        public ProductNamesAutoCompleteBoxProvider(IDataStorage dataStorage)
+        {
+            _dataStorage = dataStorage;
+        }
+
+        public IEnumerable<string> GetItems(string textPattern)
+        {
+            IEnumerable<string> results = null;
+            if (textPattern.Length > 3)
+            {
+                using (var session = _dataStorage.CreateSession())
+                {
+                    results = session.Query<ProductDescription>()
+                        .Where(p => p.Description.StartsWith(textPattern, StringComparison.OrdinalIgnoreCase))
+                        .OrderBy(p => p.Description)
+                        .Take(10)
+                        .Select(p => p.Description);
+                }
+            }
+            return results ?? new List<string>();
+        }
+
+        private readonly IDataStorage _dataStorage;
+    }
+}
