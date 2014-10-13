f7a32d452dca85bcb4b7b04be63fefdccc73130a
Luca Zenari <lozeno1982@gmail.com>
2014-10-13 22:55:04 +0100

    Implemented calculated total price columnfor datagrid

--- GestionePosizioni/PosizioniRoverfrutta/ViewModels/ProductRowViewModel.cs
+++ GestionePosizioni/PosizioniRoverfrutta/ViewModels/ProductRowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Models;
+using PosizioniRoverfrutta.Annotations;
 using PosizioniRoverfrutta.ViewModels.BaseClasses;
 
 namespace PosizioniRoverfrutta.ViewModels
 {
-    public class ProductRowViewModel : EditableModelBase<ProductRowViewModel>
+    public class ProductRowViewModel : EditableModelBase<ProductRowViewModel>, INotifyPropertyChanged
     {
         public ProductRowViewModel() : this(new ProductDetails())
         {
@@ -12,43 +15,66 @@ namespace PosizioniRoverfrutta.ViewModels
         public ProductRowViewModel(ProductDetails productDetails)
         {
             ProductDetails = productDetails;
-            UpdateTotalPrice();
         }
 
         public int? ProductId 
         {
             get { return ProductDetails.ProductId; }
-            set { ProductDetails.ProductId = value; } 
+            set
+            {
+                ProductDetails.ProductId = value;
+                OnPropertyChanged();
+            } 
         }
 
         public string Description 
         {
             get { return ProductDetails.Description; }
-            set { ProductDetails.Description = value; } 
+            set
+            {
+                ProductDetails.Description = value;
+                OnPropertyChanged();
+            } 
         }
 
         public int Pallets
         {
             get { return ProductDetails.Pallets; }
-            set { ProductDetails.Pallets = value; }
+            set
+            {
+                ProductDetails.Pallets = value;
+                OnPropertyChanged();
+            }
         }
 
         public int Packages 
         { 
             get { return ProductDetails.Packages; }
-            set { ProductDetails.Packages = value; }
+            set
+            {
+                ProductDetails.Packages = value;
+                OnPropertyChanged();
+            }
         }
 
         public decimal GrossWeight
         {
             get { return ProductDetails.GrossWeight; }
-            set { ProductDetails.GrossWeight = value; }
+            set
+            {
+                ProductDetails.GrossWeight = value;
+                OnPropertyChanged();
+            }
         }
 
         public decimal NetWeight
         {
             get { return ProductDetails.NetWeight; }
-            set { ProductDetails.NetWeight = value; }
+            set
+            {
+                ProductDetails.NetWeight = value;
+                OnPropertyChanged();
+            }
         }
 
         public decimal Price
@@ -57,14 +83,19 @@ namespace PosizioniRoverfrutta.ViewModels
             set
             {
                 ProductDetails.Price = value;
-                UpdateTotalPrice();
+                OnPropertyChanged();
+                OnPropertyChanged("TotalPrice");
             }
         }
 
         public string Currency
         {
             get { return ProductDetails.Currency; }
-            set { ProductDetails.Currency = value; }
+            set
+            {
+                ProductDetails.Currency = value;
+                OnPropertyChanged();
+            }
         }
 
         public decimal PriceParameter
@@ -73,17 +104,24 @@ namespace PosizioniRoverfrutta.ViewModels
             set
             {
                 ProductDetails.PriceParameter = value;
-                UpdateTotalPrice();
+                OnPropertyChanged();
+                OnPropertyChanged("TotalPrice");
             }
         }
 
-        public decimal TotalPrice { get; set; }
-
-        private void UpdateTotalPrice()
+        public decimal TotalPrice
         {
-            TotalPrice = Price * PriceParameter;
+            get { return Price*PriceParameter; }
         }
 
         public ProductDetails ProductDetails { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
