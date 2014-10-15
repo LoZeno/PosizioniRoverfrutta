52af38f1586fccb827c47fe61a989b286f08c753
Luca Zenari <luca.zenari@thetrainline.com>
2014-10-15 16:08:46 +0100

    Got a working and simple example of creating a PDF from HTML template with RazorEngine and iTextSharp XMLWorker!

---
+++ GestionePosizioni/ReportManager.Tests/PdfEngineTest.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using NUnit.Framework;
+
+namespace ReportManager.Tests
+{
+    [TestFixture]
+    public class PdfEngineTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var engine = new PdfEngine(new SaleConfirmation {Customer = new Customer {CompanyName = "HELLO COMPANY"}},
+                @"C:\Users\LucaZ\Documents\parsehtml.pdf");
+            engine.ExportPdf();
+        }
+    }
+}
---
+++ GestionePosizioni/ReportManager/PdfEngine.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using Models;
+using RazorEngine;
+
+namespace ReportManager
+{
+    public class PdfEngine
+    {
+        public PdfEngine(SaleConfirmation document, string path)
+        {
+            _document = document;
+            _path = path;
+        }
+
+        public void ExportPdf()
+        {
+            
+            string template;
+            using (var streamReader = new StreamReader(@"..\Debug\Templates\ConfermaVendita.cshtml", System.Text.Encoding.UTF8))
+            {
+                template = streamReader.ReadToEnd();
+            }
+
+            string htmlDocument = Razor.Parse(template, _document);
+
+            var newDocument = new Document(PageSize.A4, 80, 80, 50, 50);
+            using (var writer = PdfWriter.GetInstance(newDocument, new FileStream(_path, FileMode.Create)))
+            {
+                newDocument.Open();
+                var reader = new StringReader(htmlDocument);
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, newDocument, reader);
+                newDocument.Close();
+            }
+        }
+
+        private readonly SaleConfirmation _document;
+        private readonly string _path;
+    }
+}
