93cebda97da566959320810971f84ab89f20ac82
Luca Zenari <luca.zenari@easyjet.com>
2015-12-14 16:20:57 +0000

    First test for customer's statistics viewmodel

---
+++ GestionePosizioni/PosizioniRoverfrutta.Tests/ViewModels/Statistics/CustomerStatisticsViewModelTests.cs
@@ -0,0 +1,73 @@
+﻿using Models.Companies;
+using Models.DocumentTypes;
+using NUnit.Framework;
+using PosizioniRoverfrutta.ViewModels.Statistics;
+using QueryManager;
+using Raven.Client;
+
+namespace PosizioniRoverfrutta.Tests.ViewModels.Statistics
+{
+    [TestFixture]
+    public class CustomerStatisticsViewModelTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _dataStorage = new RavenDataStorage();
+            _dataStorage.Initialize();
+
+            using (var session = _dataStorage.CreateSession())
+            {
+                var customer = new Customer
+                {
+                    CompanyName = customerName,
+                    Address = customerAddress,
+                    City = customerCity,
+                    Country = customerCountry,
+                    EmailAddress = customerEmailAddress,
+                    PostCode = customerPostCode,
+                    StateOrProvince = customerState,
+                    VatCode = customerVatCode
+                };
+                session.Store(customer);
+
+                var priceConfirmation = new PriceConfirmation
+                {
+
+                };
+
+                session.Store(priceConfirmation);
+                session.SaveChanges();
+
+                _customerId = customer.Id;
+            }
+        }
+
+        [Test]
+        public void when_loading_the_viewmodel_it_loads_the_customer()
+        {
+            var viewModel = new CustomerStatisticsViewModel(_dataStorage, _customerId);
+
+            Assert.That(viewModel.CustomerName, Is.EqualTo(customerName));
+            Assert.That(viewModel.Address, Is.EqualTo(customerAddress));
+            Assert.That(viewModel.City, Is.EqualTo(customerCity));
+            Assert.That(viewModel.Country, Is.EqualTo(customerCountry));
+            Assert.That(viewModel.EmailAddress, Is.EqualTo(customerEmailAddress));
+            Assert.That(viewModel.PostCode, Is.EqualTo(customerPostCode));
+            Assert.That(viewModel.StateOrProvince, Is.EqualTo(customerState));
+            Assert.That(viewModel.VatCode, Is.EqualTo(customerVatCode));
+        }
+
+        private IDataStorage _dataStorage;
+        private string _customerId;
+
+        const string customerName = "Customer";
+        const string customerAddress = "Address";
+        const string customerCity = "City";
+        const string customerCountry = "Country";
+        const string customerEmailAddress = "email@address.com";
+        const string customerPostCode = "POSTCODE";
+        const string customerState = "State";
+        const string customerVatCode = "VatCode";
+    }
+}
---
+++ GestionePosizioni/PosizioniRoverfrutta/ViewModels/Statistics/CustomerStatisticsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Models.Companies;
+using PosizioniRoverfrutta.Annotations;
+using QueryManager;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PosizioniRoverfrutta.ViewModels.Statistics
+{
+    public class CustomerStatisticsViewModel : INotifyPropertyChanged
+    {
+        public CustomerStatisticsViewModel(IDataStorage dataStorage, string customerId)
+        {
+            _dataStorage = dataStorage;
+
+            using (var session = _dataStorage.CreateSession())
+            {
+                _customer = session.Load<Customer>(customerId);
+            }
+        }
+
+        public string CustomerName
+        {
+            get { return _customer.CompanyName; }
+        }
+
+        public string Address
+        {
+            get { return _customer.Address; }
+        }
+        public string City
+        {
+            get { return _customer.City; }
+        }
+        public string Country
+        {
+            get { return _customer.Country; }
+        }
+        public string EmailAddress
+        {
+            get { return _customer.EmailAddress; }
+        }
+        public string PostCode
+        {
+            get { return _customer.PostCode; }
+        }
+        public string StateOrProvince
+        {
+            get { return _customer.StateOrProvince; }
+        }
+        public string VatCode
+        {
+            get { return _customer.VatCode; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Customer _customer;
+        private IDataStorage _dataStorage;
+    }
+}
